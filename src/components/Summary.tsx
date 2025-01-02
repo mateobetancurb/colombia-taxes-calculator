1c8ab5e2fb5b560390e273bffbbadf9e74375043
Mateo Betancur <mateobetancurb@gmail.com>
2025-01-02 16:55:42 -0500

    feat: add copy to clipboard functionality in Summary component

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { currencyFormat } from "../helpers";
 
 interface SummaryProps {
@@ -5,10 +6,28 @@ interface SummaryProps {
 }
 
 function Summary({ result }: SummaryProps) {
+	const [isValueCopied, setIsValueCopied] = useState<boolean>(false);
+	async function copyToClipboard(value: number | string) {
+		try {
+			if (navigator?.clipboard?.writeText) {
+				await navigator.clipboard.writeText(value.toString());
+			}
+			setIsValueCopied(true);
+		} catch (error) {
+			console.error(error);
+		}
+	}
 	return (
 		<section className="bg-[#e0fff1] rounded-lg p-5">
 			<h2 className="font-bold text-lg text-center mb-3">Impuesto a pagar</h2>
 			<p className="text-center">{currencyFormat(result)}</p>
+			<button
+				type="button"
+				onClick={() => copyToClipboard(result)}
+				className="bg-[#16803D] text-white font-bold w-full p-2 rounded-lg mt-5"
+			>
+				{!isValueCopied ? "Copiar valor 🔖" : "Copiado ✅"}
+			</button>
 		</section>
 	);
 }
