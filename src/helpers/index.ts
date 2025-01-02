2ac8f4811f678be87c91dfe10cd02517f112dd89
Mateo Betancur <mateobetancurb@gmail.com>
2025-01-02 16:31:31 -0500

    feat: add currencyFormat helper function for Colombian Peso formatting

---
+++ src/helpers/index.ts
@@ -0,0 +1,8 @@
+export const currencyFormat = (value: number | string) => {
+	return value.toLocaleString("es-CO", {
+		style: "currency",
+		currency: "COP",
+		minimumFractionDigits: 0,
+		maximumFractionDigits: 0,
+	});
+};
