4ca0a9066125a6d8398d896e4d702fa2b4bbbf5c
Mateo Betancur <mateobetancurb@gmail.com>
2025-01-02 16:31:37 -0500

    feat: enhance Summary component to format result using currencyFormat and improve layout

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,11 +1,14 @@
+import { currencyFormat } from "../helpers";
+
 interface SummaryProps {
 	result: number | string;
 }
 
 function Summary({ result }: SummaryProps) {
 	return (
-		<section>
-			<p>{result}</p>
+		<section className="bg-[#e0fff1] rounded-lg p-5">
+			<h2 className="font-bold text-lg text-center mb-3">Impuesto a pagar</h2>
+			<p className="text-center">{currencyFormat(result)}</p>
 		</section>
 	);
 }
