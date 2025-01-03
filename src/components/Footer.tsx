ef99974c61035f1fc52f98c6e68fe675139d8801
Mateo Betancur <mateobetancurb@gmail.com>
2025-01-03 14:16:51 -0500

    feat: add Footer component to display developer information and current year

---
+++ src/components/Footer.tsx
@@ -0,0 +1,21 @@
+function Footer() {
+	const date = new Date();
+	const currentYear = date.getFullYear();
+
+	return (
+		<p className="absolute bottom-0 text-sm w-full text-center py-3">
+			Desarrollado por{" "}
+			<a
+				href="https://www.linkedin.com/in/mateobetancurb"
+				target="_blank"
+				rel="noopener noreferrer"
+				className="underline"
+			>
+				Mateo
+			</a>{" "}
+			© {currentYear}
+		</p>
+	);
+}
+
+export { Footer };
